ea098830e876858f8a7c8dbb913c9a1e9ea37b89
MoSaif00 <moh14ab@gmail.com>
2021-03-28 22:06:52 +0200

    changes files names as suggested

--- week1/react-exercises/src/perfectCustomerService.js
+++ week1/react-exercises/src/perfectCustomerService.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import guaranteeList from './GuaranteeList';
+import guaranteeList from './guaranteeList';
 const shuffle = require('lodash.shuffle');
 
 function GuaranteeDetails(props) {
